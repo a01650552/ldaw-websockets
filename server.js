f1adae88b0878867631d0cc25aa7a7cca626b4db
Alonso <a01650552@itesm.mx>
2020-04-08 21:15:57 -0500

    fin de la clase

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ let passport = require('passport');
 // Express app creation
 const app = express();
 
+//Socket.io
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 
 // Configurations
 const appConfig = require('./configs/app');
@@ -48,9 +52,23 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
+app.use('/', express.static(__dirname + '/public'));
 app.use('/', webRoutes);
 
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.emit('toast', {message: "Conectado con el servidor"});
+  let i = 0;
+  setInterval(() => {
+    socket.emit('toast', {message: "Conectado con el servidor"});
+    i++;
+  }, 10000)
+  socket.on('message-to-server', (data) => {
+    console.log('message received', data);
+  });
+});
+
 // App init
-app.listen(appConfig.expressPort, () => {
+server.listen(appConfig.expressPort, () => {
   console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
 });
